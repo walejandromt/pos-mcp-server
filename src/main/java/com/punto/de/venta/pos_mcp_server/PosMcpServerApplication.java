8669d63abc121c0e25a969d9266acd8af708bef8
Williams Alejandro <walejandromt@gmail.com>
2025-07-18 20:22:16 -0600

    first commit

---
+++ src/main/java/com/punto/de/venta/pos_mcp_server/PosMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.punto.de.venta.pos_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/punto/de/venta/pos_mcp_server/PosMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.punto.de.venta.pos_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
